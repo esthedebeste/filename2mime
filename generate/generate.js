3959da5252f89f4466cdae3c23d69631655939b5
tbhmens <57283066+tbhmens@users.noreply.github.com>
2021-09-21 10:44:46 +0200

    WIP: Comments

--- generate/generate.js
+++ generate/generate.js
@@ -1,15 +1,36 @@
 import { writeFileSync } from "fs";
 import db from "mime-db";
+/**
+ * @type {Record<string, string>}
+ */
 const result = {};
+// Map extensions to their mime-types
 for (const mime in db)
   if (db[mime].extensions)
-    for (const ext of db[mime].extensions) if (!result[ext]) result[ext] = mime;
-const valid = string => {
-  if (/^[a-zA-Z]+$/.test(string)) return true;
-  if (/^\d/.test(string)) return /^[1-9]\d*$/.test(string);
-  if (/-/.test(string)) return false;
+    for (const ext of db[mime].extensions) result[ext] ??= mime;
+/**
+ * Object key validity checker.
+ * If this function returns true, you can leave it like { key: "value" }
+ * If it returns false, { "key": "value" } is required.
+ * This is used to cut down on useless bytes.
+ * @param {string} name
+ * @returns {boolean}
+ */
+const valid = name => {
+  if (/^[a-zA-Z]+$/.test(name)) return true;
+  if (/^\d/.test(name)) return /^[1-9]\d*$/.test(name);
+  if (/-/.test(name)) return false;
   return false;
 };
+/**
+ * Some commonly occuring starts to mime-types, to
+ * short down on bytes we can execute JS at runtime to
+ * do
+ * var a = "text/"
+ * {
+ *  html: a+"html"
+ * }
+ */
 const shortcuts = Object.fromEntries(
   [
     "application/",
@@ -36,6 +57,11 @@ const shortcuts = Object.fromEntries(
     "message/vnd.",
   ].map((a, i) => [a, String.fromCharCode(97 + i)])
 );
+/**
+ * Make a shorter representation of a mime-type using the predefined {@link shortcuts}
+ * @param {string} a
+ * @returns {string}
+ */
 const shorter = a => {
   let longest = "";
   for (const shortcut in shortcuts)
@@ -45,7 +71,14 @@ const shorter = a => {
   if (longest) return `${shortcuts[longest]}+"${a.slice(longest.length)}"`;
   else return `"${a}"`;
 };
+/**
+ * Instead of writing a JSON file, we create a CJS file that exports an object.
+ * This way, we can optimize the JSON file using {@link shortcuts} and {@link valid}
+ */
 const writing =
+  /**
+   * Shortcut declarations, for { "html": a+"html" } instead of { "html": "text/html" }
+   */
   `var ` +
   Object.entries(shortcuts)
     .map(([value, name]) => `${name}=${shorter(value)}`)
@@ -53,8 +86,18 @@ const writing =
   `;module.exports={` +
   Object.entries(result)
     .sort()
+    /**
+     * Variable "" removal, for { html: a+"html" } instead of { "html": a+"html" }
+     */
     .map(([ext, value]) => `${valid(ext) ? ext : `"${ext}"`}:${shorter(value)}`)
     .join(",") +
   "}";
+/**
+ * Fun fact: writeFileSync supports URLs, so this is possible
+ */
 writeFileSync(new URL("../mime.cjs", import.meta.url), writing);
+/**
+ * Write to ./mime.json, this is used by test/test.js to test whether the outputted mime.cjs
+ * is the same as the original mime data.
+ */
 writeFileSync(new URL("./mime.json", import.meta.url), JSON.stringify(result));
---
+++ test/NOTE.md
@@ -0,0 +1,4 @@
+# Note
+
+test.js relies on filename2mime being symlinked in test/node_modules/filename2mime.
+This is so that cjs-mjs intercompatability can be tested.
