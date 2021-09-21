052930285cfc94978b55eaf99335e075f545bb32
tbhmens <57283066+tbhmens@users.noreply.github.com>
2021-09-21 17:00:34 +0200

    Include test filename2mime symlink in git

--- test/NOTE.md
+++
@@ -1,4 +0,0 @@
-# Note
-
-test.js relies on filename2mime being symlinked in test/node_modules/filename2mime.
-This is so that cjs-mjs intercompatability can be tested.
--- test/test.js
+++ test/test.js
@@ -1,20 +1,45 @@
 import { deepStrictEqual } from "assert";
-import {
-  default as defaultMime,
+import defaultMime, {
   lookup as mjsLookup,
   mime as mjsMime,
 } from "filename2mime";
 import { readFileSync } from "fs";
 import { createRequire } from "module";
-const cjs = createRequire(import.meta.url)("filename2mime");
+/**
+ * Create a cjs-like require() function to test cjs interop.
+ */
+const require = createRequire(import.meta.url);
+const cjs = require("filename2mime");
 const { mime: cjsMime, lookup: cjsLookup } = cjs;
+
 const mime = JSON.parse(
   readFileSync(new URL("../generate/mime.json", import.meta.url))
 );
-deepStrictEqual(defaultMime, mime, "1");
-deepStrictEqual(mjsMime, mime, "2");
-deepStrictEqual(cjsMime, mime, "3");
+deepStrictEqual(
+  defaultMime,
+  mime,
+  "Mime not equivalent to source. (module/default)"
+);
+deepStrictEqual(
+  mjsMime,
+  mime,
+  "Mime not equivalent to source. (module/export)"
+);
+deepStrictEqual(
+  cjsMime,
+  mime,
+  "Mime not equivalent to source. (cjs/exports.mime)"
+);
 
-deepStrictEqual(cjsLookup("/path/to/a/file.txt"), mime["txt"]);
-deepStrictEqual(mjsLookup("/path/to/a/file.txt"), mime["txt"]);
+deepStrictEqual(
+  cjsLookup("/path/to/a/file.txt"),
+  mime["txt"],
+  "Lookup issue (cjs/exports.lookup)"
+);
+deepStrictEqual(
+  mjsLookup("/path/to/a/file.txt"),
+  mime["txt"],
+  "Lookup issue (module/lookup)"
+);
+
 console.log("Tests passed");
