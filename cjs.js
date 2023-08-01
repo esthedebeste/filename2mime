79a18500341e58932b6d2db0df93c11228923b13
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2023-08-01 11:29:49 +0000

    1.3.1 - Smaller! Added a more intelligent shortcut finding algorithm (instead of hardcoded shortcuts)
    + Tabs instead of spaces & no semis

--- cjs.js
+++ cjs.js
@@ -1,13 +1,13 @@
-var mime = require("./mime.js");
-var rev = {};
+var mime = require("./mime.js"),
+	rev = {}
 for (var key in mime)
-  if (rev[mime[key]]) rev[mime[key]].push(key);
-  else rev[mime[key]] = [key];
+	if (rev[mime[key]]) rev[mime[key]].push(key)
+	else rev[mime[key]] = [key]
 function lookup(f) {
-  return mime[f.slice(f.lastIndexOf(".") + 1)];
+	return mime[f.slice(f.lastIndexOf(".") + 1)]
 }
 module.exports = {
-  mime: mime,
-  reverse: rev,
-  lookup: lookup,
-};
+	mime: mime,
+	reverse: rev,
+	lookup: lookup,
+}
--- generate/generate.js
+++ generate/generate.js
@@ -1,13 +1,15 @@
-import { writeFileSync } from "fs";
-import db from "mime-db";
+import { writeFileSync } from "fs"
+import db from "mime-db"
+
 /**
+ * The value that will be compacted and written to mime.js. This is a map of extensions to mime-types.
  * @type {Record<string, string>}
  */
-const result = {};
+const result = {}
 // Map extensions to their mime-types
 for (const mime in db)
-  if (db[mime].extensions)
-    for (const ext of db[mime].extensions) result[ext.toLowerCase()] ??= mime;
+	if (db[mime].extensions)
+		for (const ext of db[mime].extensions) result[ext.toLowerCase()] ??= mime
 /**
  * Object key validity checker.
  * If this function returns true, you can leave it like { key: "value" }
@@ -16,91 +18,131 @@ for (const mime in db)
  * @param {string} name
  * @returns {boolean}
  */
-const valid = name => {
-  if (/^[a-zA-Z]+$/.test(name)) return true;
-  if (/^\d/.test(name)) return /^[1-9]\d*$/.test(name);
-  if (/-/.test(name)) return false;
-  return false;
-};
+const valid = (name) => {
+	if (/^[a-zA-Z]+$/.test(name)) return true
+	if (/^\d/.test(name)) return /^[1-9]\d*$/.test(name)
+	if (/-/.test(name)) return false
+	return false
+}
+
+/** @type {Map<string, number>} */
+const bytesCutShortcutCache = new Map()
 /**
- * Some commonly occuring starts to mime-types, to
- * short down on bytes we can execute JS at runtime to
- * do
- * var a = "text/"
- * {
- *  html: a+"html"
- * }
+ * How many bytes could we save if this string was shortcutted?
+ * @param {string} shortcut
+ * @returns {number}
  */
-const shortcuts = Object.fromEntries(
-  [
-    "application/",
-    "application/vnd.",
-    "application/x-",
-    "image/",
-    "image/vnd.",
-    "image/x-",
-    "text/",
-    "text/vnd.",
-    "text/x-",
-    "video/",
-    "video/vnd.",
-    "video/x-",
-    "audio/",
-    "audio/vnd.",
-    "audio/x-",
-    "model/",
-    "model/vnd.",
-    "chemical/",
-    "chemical/x-",
-    "font/",
-    "message/",
-    "message/vnd."
-  ].map((a, i) => [a, String.fromCharCode(97 + i)])
-);
+function bytesCutShortcut(shortcut) {
+	if (bytesCutShortcutCache.has(shortcut))
+		return bytesCutShortcutCache.get(shortcut)
+	let bytes = 0
+	for (const ext in result)
+		if (result[ext] === shortcut) bytes += shortcut.length
+		else if (result[ext].startsWith(shortcut) || result[ext].endsWith(shortcut))
+			bytes += shortcut.length - 3 // aa+"html" instead of "text/html"
+	bytesCutShortcutCache.set(shortcut, bytes)
+	return bytes
+}
+
 /**
- * Make a shorter representation of a mime-type using the predefined {@link shortcuts}
- * @param {string} a
+ * @type {Array<{ value: number, full: string, used: boolean }>}
+ */
+let shortcuts = []
+for (const extension in result) {
+	const mime = result[extension]
+	for (let i = mime.length; i > 1; i--) {
+		const shortcut = mime.slice(0, i)
+		const bytes = bytesCutShortcut(shortcut)
+		if (
+			bytes >
+			shortcut.length + 6 // ,aa="shortcut"
+		)
+			shortcuts.push({ value: bytes, full: shortcut })
+	}
+}
+shortcuts.sort((a, b) => b.value - a.value) // Sort by value, descending
+// Remove duplicates
+shortcuts = shortcuts.filter(
+	(a) => shortcuts.find((b) => b.full === a.full) === a
+)
+
+/**
+ * Convert a number to a valid javascript identifier
+ * @param {number} number
  * @returns {string}
  */
-const shorter = a => {
-  let longest = "";
-  for (const shortcut in shortcuts)
-    if (a === shortcut) continue;
-    else if (a.startsWith(shortcut))
-      if (shortcut.length > longest.length) longest = shortcut;
-  if (longest) return `${shortcuts[longest]}+"${a.slice(longest.length)}"`;
-  else return `"${a}"`;
-};
+function numberToIdentifier(number) {
+	const ALPHABET_FIRST = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$"
+	let identifier = ""
+	identifier += ALPHABET_FIRST[number % ALPHABET_FIRST.length]
+	number = Math.floor(number / ALPHABET_FIRST.length)
+	const ALPHABET = ALPHABET_FIRST + "_0123456789"
+	while (number > 0) {
+		identifier += ALPHABET[number % ALPHABET.length]
+		number = Math.floor(number / ALPHABET.length)
+	}
+	return identifier
+}
+
+/**
+ * Make a shorter representation of a mime-type using the predefined {@link shortcuts}
+ * @param {string} mime
+ * @param {string} [exclude] - Exclude this shortcut from being used (used when shortening shortcuts)
+ * @returns {string}
+ */
+const shorter = (mime, exclude = undefined) => {
+	let longest = undefined
+	for (const shortcut of shortcuts)
+		if (exclude === shortcut) break
+		else if (mime.startsWith(shortcut.full))
+			if (longest == null || shortcut.full.length > longest.full.length)
+				longest = shortcut
+	if (longest != null) {
+		longest.used = true
+		if (longest.full === mime)
+			// { html: h } instead of { html: h+"" }
+			return numberToIdentifier(shortcuts.indexOf(longest))
+		return `${numberToIdentifier(shortcuts.indexOf(longest))}+"${mime.slice(
+			longest.full.length
+		)}"`
+	} else return `"${mime}"`
+}
+
+// Shortcut all mime-types to see what shortcuts will actually be used
+for (const mime of Object.values(result)) shorter(mime)
+// Remove unused shortcuts
+shortcuts = shortcuts.filter((a) => a.used)
+
 /**
  * Instead of writing a JSON file, we create a CJS file that exports an object.
  * This way, we can optimize the JSON file using {@link shortcuts} and {@link valid}
  */
 const writing =
-  /**
-   * Shortcut declarations, for { "html": a+"html" } instead of { "html": "text/html" }
-   */
-  `var ` +
-  Object.entries(shortcuts)
-    .map(([value, name]) => `${name}=${shorter(value)}`)
-    .join(",") +
-  `;module.exports={` +
-  Object.entries(result)
-    .sort()
-    /**
-     * Variable "" removal, for { html: a+"html" } instead of { "html": a+"html" }
-     */
-    .map(([ext, value]) => `${valid(ext) ? ext : `"${ext}"`}:${shorter(value)}`)
-    .join(",") +
-  "}";
-/**
- * Fun fact: writeFileSync supports URLs, so this is possible
- */
-writeFileSync(new URL("../mime.js", import.meta.url), writing);
+	/** Shortcut declarations, for { "html": a+"html" } instead of { "html": "text/html" } */
+	`var ` +
+	shortcuts
+		.map(
+			(shortcut, i) =>
+				`${numberToIdentifier(i)}=${shorter(shortcut.full, shortcut)}`
+		)
+		.join(",") +
+	`;module.exports={` +
+	Object.entries(result)
+		.sort()
+		/**
+		 * Variable "" removal, for { html: a+"html" } instead of { "html": a+"html" }
+		 */
+		.map(([ext, value]) => `${valid(ext) ? ext : `"${ext}"`}:${shorter(value)}`)
+		.join(",") +
+	"}"
+
+/** Fun fact: writeFileSync supports URLs, so this is possible */
+writeFileSync(new URL("../mime.js", import.meta.url), writing)
 /**
  * Write to generate/mime.json, this is used by test/test.js to test whether the outputted mime.js
  * is the same as the original mime data.
  */
 writeFileSync(
-  new URL("./mime.json", import.meta.url),
-  JSON.stringify(Object.fromEntries(Object.entries(result).sort()))
-);
+	new URL("./mime.json", import.meta.url),
+	JSON.stringify(Object.fromEntries(Object.entries(result).sort()))
+)
